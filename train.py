741966bf9aefa9eed8a759b5e44280c78d27af6b
mudigosa <shanmukhamudigonda@gmail.com>
2019-01-27 17:35:45 -0500

    Update train.py

--- train.py
+++ train.py
@@ -13,39 +13,39 @@ import argparse
 
 import futils
 
-ap = argparse.ArgumentParser(description='Train.py')
+argumentparser = argparse.ArgumentParser(description='Train.py')
 # Command Line ardguments
 
-ap.add_argument('data_dir', nargs='*', action="store", default="./flowers/")
-ap.add_argument('--gpu', dest="gpu", action="store", default="gpu")
-ap.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
-ap.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
-ap.add_argument('--dropout', dest = "dropout", action = "store", default = 0.5)
-ap.add_argument('--epochs', dest="epochs", action="store", type=int, default=1)
-ap.add_argument('--arch', dest="arch", action="store", default="vgg16", type = str)
-ap.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
+argumentparser.add_argument('data_dir', nargs='*', action="store", default="./flowers/")
+argumentparser.add_argument('--gpu', dest="gpu", action="store", default="gpu")
+argumentparser.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
+argumentparser.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
+argumentparser.add_argument('--dropout', dest = "dropout", action = "store", default = 0.5)
+argumentparser.add_argument('--epochs', dest="epochs", action="store", type=int, default=1)
+argumentparser.add_argument('--arch', dest="arch", action="store", default="vgg16", type = str)
+argumentparser.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
 
-pa = ap.parse_args()
-where = pa.data_dir
-path = pa.save_dir
-lr = pa.learning_rate
-structure = pa.arch
-dropout = pa.dropout
-hidden_layer1 = pa.hidden_units
-power = pa.gpu
-epochs = pa.epochs
+parser = argumentparser.parse_args()
+where = parser.data_dir
+paths = parser.save_dir
+lr = parser.learning_rate
+structures = parser.arch
+dropout = parser.dropout
+hidden_layer = parser.hidden_units
+power = parser.gpu
+epochs = parser.epochs
 
 
 trainloader, v_loader, testloader = futils.load_data(where)
 
 
-model, optimizer, criterion = futils.nn_setup(structure,dropout,hidden_layer1,lr,power)
+model, optimizer, criterion = futils.nn_setup(structures,dropout,hidden_layer,lr,power)
 
 
 futils.train_network(model, optimizer, criterion, epochs, 20, trainloader, power)
 
 
-futils.save_checkpoint(path,structure,hidden_layer1,dropout,lr)
+futils.save_checkpoint(paths,structures,hidden_layer,dropout,lr)
 
 
 print("All Set and Done. The Model is trained") # Coffee timeee
