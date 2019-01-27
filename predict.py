b30113f426d249940be8f64b7d04d1c1c52a511a
mudigosa <shanmukhamudigonda@gmail.com>
2019-01-27 02:20:03 -0500

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,109 +1,60 @@
-import torch
-from torch.autograd import Variable
-import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 import numpy as np
-
-from PIL import Image
-from train import load_model
-
+import torch
+from torch import nn
+from torch import tensor
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import torchvision.models as models
+from collections import OrderedDict
 import json
+import PIL
+from PIL import Image
 import argparse
 
-# Define command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--image', type=str, help='Image to predict')
-parser.add_argument('--checkpoint', type=str, help='Model checkpoint to use when predicting')
-parser.add_argument('--topk', type=int, help='Return top K predictions')
-parser.add_argument('--labels', type=str, help='JSON file containing label names')
-parser.add_argument('--gpu', action='store_true', help='Use GPU if available')
+import futils
 
-args, _ = parser.parse_known_args()
+#Command Line Arguments
 
+ap = argparse.ArgumentParser(
+    description='predict-file')
+ap.add_argument('input_img', default='paind-project/flowers/test/1/image_06752.jpg', nargs='*', action="store", type = str)
+ap.add_argument('checkpoint', default='/home/workspace/paind-project/checkpoint.pth', nargs='*', action="store",type = str)
+ap.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
+ap.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
+ap.add_argument('--gpu', default="gpu", action="store", dest="gpu")
 
-# Implement the code to predict the class from an image file
-def predict(image, checkpoint, topk=5, labels='', gpu=False):
-    ''' Predict the class (or classes) of an image using a trained deep learning model.
-    '''
-    # Use command line values when specified
-    if args.image:
-        image = args.image     
-        
-    if args.checkpoint:
-        checkpoint = args.checkpoint
-
-    if args.topk:
-        topk = args.topk
-            
-    if args.labels:
-        labels = args.labels
-
-    if args.gpu:
-        gpu = args.gpu
-    
-    # Load the checkpoint
-    checkpoint_dict = torch.load(checkpoint)
-
-    arch = checkpoint_dict['arch']
-    num_labels = len(checkpoint_dict['class_to_idx'])
-    hidden_units = checkpoint_dict['hidden_units']
-        
-    model = load_model(arch=arch, num_labels=num_labels, hidden_units=hidden_units)
-
-    # Use gpu if selected and available
-    if gpu and torch.cuda.is_available():
-        model.cuda()
-        
-    was_training = model.training    
-    model.eval()
-    
-    img_loader = transforms.Compose([
-        transforms.Resize(256), 
-        transforms.CenterCrop(224), 
-        transforms.ToTensor()])
-    
-    pil_image = Image.open(image)
-    pil_image = img_loader(pil_image).float()
-    
-    image = np.array(pil_image)    
-    
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    image = (np.transpose(image, (1, 2, 0)) - mean)/std    
-    image = np.transpose(image, (2, 0, 1))
-    
-    image = Variable(torch.FloatTensor(image), requires_grad=True)
-    image = image.unsqueeze(0) # this is for VGG
-    
-    if gpu and torch.cuda.is_available():
-        image = image.cuda()
-            
-    result = model(image).topk(topk)
-
-    if gpu and torch.cuda.is_available():
-        # Added softmax here as per described here:
-        # https://github.com/pytorch/vision/issues/432#issuecomment-368330817
-        probs = torch.nn.functional.softmax(result[0].data, dim=1).cpu().numpy()[0]
-        classes = result[1].data.cpu().numpy()[0]
-    else:       
-        probs = torch.nn.functional.softmax(result[0].data, dim=1).numpy()[0]
-        classes = result[1].data.numpy()[0]
+pa = ap.parse_args()
+path_image = pa.input_img
+number_of_outputs = pa.top_k
+power = pa.gpu
+input_img = pa.input_img
+path = pa.checkpoint
 
 
-    if labels:
-        with open(labels, 'r') as f:
-            cat_to_name = json.load(f)
 
-        labels = list(cat_to_name.values())
-        classes = [labels[x] for x in classes]
-        
-    model.train(mode=was_training)
+training_loader, testing_loader, validation_loader = futils.load_data()
 
-    # Print only when invoked by command line 
-    if args.image:
-        print('Predictions and probabilities:', list(zip(classes, probs)))
-    
-    return probs, classes
 
-# Perform predictions if invoked from command line
-if args.image and args.checkpoint:
-predict(args.image, args.checkpoint)
+futils.load_checkpoint(path)
+
+
+with open('cat_to_name.json', 'r') as json_file:
+    cat_to_name = json.load(json_file)
+
+
+probabilities = futils.predict(path_image, model, number_of_outputs, power)
+
+
+labels = [cat_to_name[str(index + 1)] for index in np.array(probabilities[1][0])]
+probability = np.array(probabilities[0][0])
+
+
+i=0
+while i < number_of_outputs:
+    print("{} with a probability of {}".format(labels[i], probability[i]))
+    i += 1
+
+print("Here you are")
--- train.py
+++ train.py
@@ -1,248 +1,51 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.optim.lr_scheduler as lr_scheduler
-import torch.utils.data as data
-
-
-import torchvision.models as models
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-
+import matplotlib.pyplot as plt
+import numpy as np
 import time
-import copy
+import torch
+from torch import nn
+from torch import tensor
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import torchvision.models as models
 import argparse
 
-# Define command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', type=str, help='Path to dataset ')
-parser.add_argument('--gpu', action='store_true', help='Use GPU if available')
-parser.add_argument('--epochs', type=int, help='Number of epochs')
-parser.add_argument('--arch', type=str, help='Model architecture')
-parser.add_argument('--learning_rate', type=float, help='Learning rate')
-parser.add_argument('--hidden_units', type=int, help='Number of hidden units')
-parser.add_argument('--checkpoint', type=str, help='Save trained model checkpoint to file')
+import futils
+
+ap = argparse.ArgumentParser(description='Train.py')
+# Command Line ardguments
+
+ap.add_argument('data_dir', nargs='*', action="store", default="./flowers/")
+ap.add_argument('--gpu', dest="gpu", action="store", default="gpu")
+ap.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
+ap.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
+ap.add_argument('--dropout', dest = "dropout", action = "store", default = 0.5)
+ap.add_argument('--epochs', dest="epochs", action="store", type=int, default=1)
+ap.add_argument('--arch', dest="arch", action="store", default="vgg16", type = str)
+ap.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
+
+pa = ap.parse_args()
+where = pa.data_dir
+path = pa.save_dir
+lr = pa.learning_rate
+structure = pa.arch
+dropout = pa.dropout
+hidden_layer1 = pa.hidden_units
+power = pa.gpu
+epochs = pa.epochs
 
 
-
-args, _ = parser.parse_known_args()
-
-# This method loads and tunes in a model
-def load_model(arch='vgg19', num_labels=102, hidden_units=4096):
-    # Load a pre-trained model
-    if arch=='vgg19':
-        # Load a pre-trained model
-        model = models.vgg19(pretrained=True)
-    elif arch=='alexnet':
-        model = models.alexnet(pretrained=True)
-    else:
-        raise ValueError('Unexpected network architecture', arch)
-        
-    # Freeze its parameters
-    for param in model.parameters():
-        param.requires_grad = False
-    
-    # Features, removing the last layer
-    features = list(model.classifier.children())[:-1]
-  
-    # Number of filters in the bottleneck layer
-    num_filters = model.classifier[len(features)].in_features
-
-    # Extend the existing architecture with new layers
-    features.extend([
-        nn.Dropout(),
-        nn.Linear(num_filters, hidden_units),
-        nn.ReLU(True),
-        nn.Dropout(),
-        nn.Linear(hidden_units, hidden_units),
-        nn.ReLU(True),
-        nn.Linear(hidden_units, num_labels),
-        ##nn.Softmax(dim=1) 
-        # Please, notice Softmax layer has not been added as per Pytorch answer:
-        # https://github.com/pytorch/vision/issues/432#issuecomment-368330817
-        # It is not either included in its transfer learning tutorial:
-        # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-    ])
-    
-    model.classifier = nn.Sequential(*features)
-
-    return model
+trainloader, v_loader, testloader = futils.load_data(where)
 
 
-
-# This method trains a model
-def train_model(image_datasets, arch='vgg19', hidden_units=4096, epochs=25, learning_rate=0.001, gpu=False, checkpoint=''):
-    # Use command line values when specified
-    if args.arch:
-        arch = args.arch     
-        
-    if args.hidden_units:
-        hidden_units = args.hidden_units
-
-    if args.epochs:
-        epochs = args.epochs
-            
-    if args.learning_rate:
-        learning_rate = args.learning_rate
-
-    if args.gpu:
-        gpu = args.gpu
-
-    if args.checkpoint:
-        checkpoint = args.checkpoint        
-        
-    # Using the image datasets, define the dataloaders
-    dataloaders = {
-        x: data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=2)
-        for x in list(image_datasets.keys())
-    }
- 
-    # Calculate dataset sizes.
-    dataset_sizes = {
-        x: len(dataloaders[x].dataset) 
-        for x in list(image_datasets.keys())
-    }    
-
-        
-    print('Network architecture:', arch)
-    print('Number of hidden units:', hidden_units)
-    print('Number of epochs:', epochs)
-    print('Learning rate:', learning_rate)
-
-    # Load the model     
-    num_labels = len(image_datasets['train'].classes)
-    model = load_model(arch=arch, num_labels=num_labels, hidden_units=hidden_units)
-
-    # Use gpu if selected and available
-    if gpu and torch.cuda.is_available():
-        print('Using GPU for training')
-        device = torch.device("cuda:0")
-        model.cuda()
-    else:
-        print('Using CPU for training')
-        device = torch.device("cpu")     
-
-                
-    # Defining criterion, optimizer and scheduler
-    # Observe that only parameters that require gradients are optimized
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(list(filter(lambda p: p.requires_grad, model.parameters())), lr=learning_rate, momentum=0.9)
-    scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)    
-        
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    for epoch in range(epochs):
-        print('Epoch {}/{}'.format(epoch + 1, epochs))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'valid']:
-            if phase == 'train':
-                scheduler.step()
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:                
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            if phase == 'valid' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-
-    # Load best model weights
-    model.load_state_dict(best_model_wts)
-    
-    # Store class_to_idx into a model property
-    model.class_to_idx = image_datasets['train'].class_to_idx
-    
-    # Save checkpoint if requested
-    if checkpoint:
-        print ('Saving checkpoint to:', checkpoint) 
-        checkpoint_dict = {
-            'arch': arch,
-            'class_to_idx': model.class_to_idx, 
-            'state_dict': model.state_dict(),
-            'hidden_units': hidden_units
-        }
-        
-        torch.save(checkpoint_dict, checkpoint)
-    
-    # Return the model
-    return model
+model, optimizer, criterion = futils.nn_setup(structure,dropout,hidden_layer1,lr,power)
 
 
-# Train model if invoked from command line
-if args.data_dir:    
-    # Default transforms for the training, validation, and testing sets
-    data_transforms = {
-        'train': transforms.Compose([
-            transforms.RandomRotation(45),
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-         ]),
-        'valid': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-         ]),
-        'test': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-         ])
-    }
-    
-    # Load the datasets with ImageFolder
-    image_datasets = {
-        x: datasets.ImageFolder(root=args.data_dir + '/' + x, transform=data_transforms[x])
-        for x in list(data_transforms.keys())
-    }
-        
-train_model(image_datasets) 
+futils.train_network(model, optimizer, criterion, epochs, 20, trainloader, power)
+
+
+futils.save_checkpoint(path,structure,hidden_layer1,dropout,lr)
+
+
+print("All Set and Done. The Model is trained") # Coffee timeee
